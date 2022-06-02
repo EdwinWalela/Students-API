f0597bac46fd0b13e0719d474d6c5b6c7a8318c6
edwinwalela <edwinwalela@gmail.com>
2022-06-02 15:32:19 +0300

    save students

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /*
@@ -28,6 +30,12 @@ public class StudentController {
   @GetMapping
   public List<Student> getStudents(){
     return studentService.getStudents();
+  }
+
+  @PostMapping
+  // @RequestBody Class -> Maps request body to class
+  public void registerStudent(@RequestBody Student student){
+    studentService.createStudent(student);
   }
 }
 
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,7 +31,12 @@ public class StudentService {
 		return students;
 	}
 
-	public void createStudent(){
-		
+	public void createStudent(Student student){
+		Optional<Student> exists = studentRepository.findStudentByEmail(student.getEmail());
+		if(exists.isPresent()){
+			System.out.println("Email in use");
+		}else{
+			studentRepository.save(student);
+		}
 	}
 }
