8d9eb3b59f1036fa1d96e40c85708c684062a688
edwinwalela <edwinwalela@gmail.com>
2022-06-02 12:58:30 +0300

    db connection

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,7 +2,28 @@ package com.example.demo.student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity // Mark this class as an entity
+@Table(name = "students") // Map Class to DB Table
 public class Student {
+  @Id
+  @SequenceGenerator(
+    name = "student_sequence",
+    sequenceName = "student_sequence",
+    allocationSize = 1
+  )
+
+  @GeneratedValue(
+    strategy = GenerationType.SEQUENCE,
+    generator = "student_sequence"
+
+  )
   private Long id;
   private String name;
   private Integer age;
